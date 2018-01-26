833c3fc647826d6a7e1dd0cf919f793c07f5a31e
bryancasler <bryan.casler@gmail.com>
2018-01-26 15:13:50 -0500

    Removed delay

--- Helper Scripts/4site-en-form-auto-submitter.js
+++ Helper Scripts/4site-en-form-auto-submitter.js
@@ -1,5 +1,4 @@
 // 4Site Studios helper script for automatically a form on Engaging Networks when "en-auto-submit=1" is present in URL
-
 // Alternate code using mutation observer to potentially fire sooner: https://pastebin.com/raw/kReLvTL6
 
 function getUrlParam(name) {
@@ -7,26 +6,14 @@ function getUrlParam(name) {
     return (results && results[1]) || undefined;
 }
 
-
 var id = getUrlParam('en-auto-submit');
 if (id > "") {
 
-    // The en-auto-submit argument is present in the URL
-    //document.addEventListener('DOMContentLoaded',function(){
-    
-    console.log("en-aut-submit present in URL")
     window.addEventListener("load", function(event) {
+	
+	document.querySelector('.en__submit button').click();
+	console.log("en-aut-submit present in URL, submit button auto clicked")
 
-    	console.log("Window loaded, waiting 500ms before clicking submit button")
-
-		// Pause briefly to prevent EN "Request already submitted" errors
-		setTimeout(function() {
-			
-			document.querySelector('.en__submit button').click();
-			console.log("Submit button clicked")    
-
-		}, 500);
-		
-	});
+    });
 
 }
