71a003f7725adc42553fde778fd8bf8bf980ad4d
Bryan Casler <bryan.casler@gmail.com>
2018-01-28 23:48:06 -0500

    Update to JS Structure

--- Helper Scripts/4site-en-helper-functions.js
+++ Helper Scripts/4site-en-helper-functions.js
@@ -17,3 +17,13 @@ if (id > "") {
     });
 
 }
+
+// Resizes parent iFrame any time a child iframe is clicked. Overkill but a quick fix
+document.onclick = function(){
+    
+    // Wait a moment before triggering so any visual page redraw can happen and the final correct dimension are sent
+    setTimeout(function() {
+        pymChild.sendHeight();
+    }, 25);
+            
+};    
