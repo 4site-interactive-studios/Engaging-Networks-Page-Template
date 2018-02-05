bb24c380d0cb91e14844b4e764895307c8c5b7c1
Bryan Casler <bryan.casler@gmail.com>
2018-02-05 13:55:08 -0500

    Cleaned up healper script code

--- Helper Scripts/4site-en-helper-functions.js
+++ Helper Scripts/4site-en-helper-functions.js
@@ -1,29 +1,30 @@
-// 4Site Studios helper script for automatically a form on Engaging Networks when "en-auto-submit=1" is present in URL
-// Alternate code using mutation observer to potentially fire sooner: https://pastebin.com/raw/kReLvTL6
-
+// Provides a function for returning the value of a URL parameter
+// e.g. var id = getUrlParam('en-auto-submit');
 function getUrlParam(name) {
     var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
     return (results && results[1]) || undefined;
 }
 
+// 4Site Studios helper script for automatically submitting a form on Engaging Networks. Works when "en-auto-submit=1" is present in URL
+// Alternate code using mutation observer to potentially fire sooner: https://pastebin.com/raw/kReLvTL6
 var id = getUrlParam('en-auto-submit');
 if (id > "") {
-
     window.addEventListener("load", function(event) {
-	
-	document.querySelector('.en__submit button').click();
-	console.log("en-aut-submit present in URL, submit button auto clicked")
-
+        document.querySelector('.en__submit button').click();
+        // Debug 
+        // console.log("en-auto-submit present in URL, submit button has been auto clicked")
     });
-
 }
 
-// Resizes parent iFrame any time a child iframe is clicked. Overkill but a quick fix
-document.onclick = function(){
-    
-    // Wait a moment before triggering so any visual page redraw can happen and the final correct dimension are sent
+// Utilizing PYM responsive iFrame library, resizes parent iFrame any time a child iframe is clicked. This is overkill but a quick fix that solves for most use cases.
+document.onclick = function() {
+
+    // Send height update immedietely. 
+    pymChild.sendHeight();
+
+    // And then wait a moment before triggering again so any visual page redraw is corrected for.
     setTimeout(function() {
         pymChild.sendHeight();
     }, 25);
-            
-};    
+
+};
