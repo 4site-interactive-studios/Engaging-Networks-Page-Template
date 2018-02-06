b8cf2d3071246b97e4f69fea485b106d7fd655e8
Bryan Casler <bryan.casler@gmail.com>
2018-02-06 17:07:42 -0500

    Added more helper script comments and cleaned up code

--- helpers/helper-functions.js
+++ helpers/helper-functions.js
@@ -3,25 +3,38 @@
  * Project: TBD
  ***********************************/
 
+/************************************
+ * Function Calls
+ ***********************************/
+
 // Provides a function for returning the value of a URL parameter
 // e.g. var id = getUrlParam('en-auto-submit');
 function getUrlParam(name) {
-    window.addEventListener("load", function(event) {
-        var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        return (results && results[1]) || undefined;
-    });
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return (results && results[1]) || undefined;
 }
 
-window.addEventListener("load", function(event) {
+/************************************
+ * On "DOMContentLoaded"
+ * The whole document (HTML) has been loaded.
+ ***********************************/
 
+window.addEventListener("DOMContentLoaded", function(event) {
     // 4Site Studios helper script for automatically submitting a form on Engaging Networks. Works when "en-auto-submit=1" is present in URL
     // Alternate code using mutation observer to potentially fire sooner: https://pastebin.com/raw/kReLvTL6
     var id = getUrlParam('en-auto-submit');
-    
-        if (id > "") {
+    if (id > "") {
         document.querySelector('.en__submit button').click();
 
     }
+});
+
+/************************************
+ * On "Load"
+ * The whole document (HTML) and its resources (e.g. images, iframes, scripts) have been loaded.
+ ***********************************/
+
+window.addEventListener("load", function(event) {
 
     // Utilizing PYM responsive iFrame library, resizes parent iFrame any time a child iframe is clicked. This is overkill but a quick fix that solves for most use cases.
     document.onclick = function() {
@@ -33,17 +46,14 @@ window.addEventListener("load", function(event) {
         setTimeout(function() {
             pymChild.sendHeight();
         }, 25);
-
     };
 
     // The Auto Submit argument is present in the local storage, click the submit button.
     if (typeof(Storage) !== "undefined") {
-      if(localStorage.quickSubmit == 'true') {
-        
-        // The Auto Submit argument is present in the local storage
-        document.querySelector('#en_actionMessageToggle_SubmitButton button').click();   
-             
-      }
+        if (localStorage.quickSubmit == 'true') {
+            // The Auto Submit argument is present in the local storage
+            document.querySelector('#en_actionMessageToggle_SubmitButton button').click();
+        }
     }
 
 });
